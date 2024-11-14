1cf481a508ec5aa6d339e96114196ed0306b70da
Jesus-Esquivel <a.22308051281176@cbtis128.edu.mx>
2024-11-14 13:26:16 -0600

    Add files via upload

--- UIII_P1_Tabla_Materia_CRUD_v4/materia_app/urls.py
+++ UIII_P1_Tabla_Materia_CRUD_v4/materia_app/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path("",views.inicio_vista,name="inicio_vista"),
     path("registrarMateria/",views.registrarMateria,name="registrarMateria"),    
     path("seleccionarMateria/<codigo>",views.seleccionarMateria,name="seleccionarMateria"),
-    path("editarMateria/",views.editarMateria, name="editarMateria"),
+    path("editarMateria/<codigo>",views.editarMateria,name="editarMateria"),
     path("borrarMateria/<codigo>",views.borrarMateria,name="borrarMateria")
 
 ]
