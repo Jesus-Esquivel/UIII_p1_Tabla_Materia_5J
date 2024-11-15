3cf76bb946485eb6881ea0765d1f330dd6f1511d
Jesus-Esquivel <a.22308051281176@cbtis128.edu.mx>
2024-11-14 21:45:44 -0600

    Add files via upload

--- UIII_P1_Tabla_Materia_CRUD_v4/materia_app/views.py
+++ UIII_P1_Tabla_Materia_CRUD_v4/materia_app/views.py
@@ -17,22 +17,23 @@ def registrarMateria(request):
 
     return redirect("/")
 
-def seleccionarMateria(request,codigo):
+def SeleccionarMateria(request,codigo):
     materia=Materia.objects.get(codigo=codigo)
     return render(request,"editarmateria.html",{"mismaterias":materia})
 
-def editarMateria(request,codigo):
+def editarMateria(request):
     codigo=request.POST["txtcodigo"]
     nombre=request.POST["txtnombre"]
     creditos=request.POST["numcreditos"]
     materia=Materia.objects.get(codigo=codigo)
     materia.nombre=nombre
     materia.creditos=creditos
-    materia.save()# Guarda registro actualizador
+    materia.save() #Guarda Registro Actualizado
     return redirect("/")
 
 def borrarMateria(request,codigo):
     materia=Materia.objects.get(codigo=codigo)
-    materia.delete()# borra el registro
+    materia.delete() #Borrar El Registro
     return redirect("/")
 
+    
